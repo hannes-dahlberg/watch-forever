5d3ac47634ea98fd020ff8417185003bd6d935b7
Hannes Dahlberg <hannes@dwik.se>
2019-01-23 16:58:30 +0100

    Removes forever json and updates instance controller

--- api/controllers/instances.controller.ts
+++ api/controllers/instances.controller.ts
@@ -1,16 +1,21 @@
+import { ConfigService, container } from "artos";
 import * as childProcess from "child_process";
 import { Request, RequestHandler, Response } from "express";
+import * as fs from "fs";
 import { InstancesDTO } from "../../shared/dto";
+
+const configService: ConfigService = container.getService(ConfigService);
 
 export class InstancesController {
   public index(): RequestHandler {
     return (request: Request, response: Response): void => {
-      childProcess.exec(`find .. -maxdepth 2 -name "forever.json"`, (error: any, stdout: string) => {
+      childProcess.exec(`find ${configService.get("INSTANCES_ROOT", "..")} -maxdepth ${configService.get("INSTANCES_DEPTH", "2")} -name "forever.json"`, (error: any, stdout: string) => {
         if (error) { response.sendStatus(500); return; }
         const list = stdout.split("\n");
         if (list.length === 0) { response.sendStatus(500); return; }
+
         response.json(list.filter((instance: string) => instance !== "").map((instance: string) => InstancesDTO.parse({
-          project: instance.split("/")[1],
+          project: JSON.parse(fs.readFileSync(instance, "utf8")).uid,
           path: instance,
         }).serialize()));
       });
